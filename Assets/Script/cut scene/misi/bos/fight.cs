12ad7ab24967541d9337ffc6c8dcac9b80b901df
Tamzki <ihsansandi625@gmail.com>
2026-08-11 22:27:52 +0700

    Update feature player

--- Assets/Script/cut scene/misi/bos/fight.cs
+++ Assets/Script/cut scene/misi/bos/fight.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class fight : MonoBehaviour
 {
@@ -27,8 +28,10 @@ public class fight : MonoBehaviour
 
         if (stamina <= 0)
         {
-            NPCPatrol1.instance.StartPatrol();
-            NPCPatrol.instance.kalah();
+        int win = PlayerPrefs.GetInt("win", 1);
+        
+        PlayerPrefs.SetInt("winning" + win, 1);  
+            SceneManager.LoadScene(SceneData.misi);
         }  
     }
 
--- Assets/Script/sampah/Spawner.cs
+++ Assets/Script/sampah/Spawner.cs
@@ -4,19 +4,23 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
+    public static Spawner instance;
     public GameObject[] trashPrefabs;
     public Transform[] spawn;
     int minT = 5;
     int Maxt = 6;
 
-    public MiniBoss miniBoss;
+    public bool smph;
 
+    void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
         SpawnTrash();
     }
-
 
     IEnumerator LoopSampah()
     {
@@ -26,22 +30,20 @@ public class Spawner : MonoBehaviour
         
             if (statusKuis != 1)
         {
+            Debug.Log("Sampah tidak akan muncul karena kuis belum selesai.");
             yield break;
         }
 
-        while (!miniBoss.miniBossKalah)
+        while (smph)
         {
             SpawnTrash();
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(30f);
         }
     }
 
 
     void SpawnTrash()
     {
-     
-        
-
                 int jumlah = Random.Range(minT,Maxt + 1);
                 for (int i = 0; i < jumlah; i++)
                 {
